0f99b1238f555b5dda2eec6ee98e5ea2b22309d7
Naveena Somu <nsomu1@binghamton.edu>
2022-03-12 23:42:36 -0500

    project2

--- submit/prj1-sol/knn-ocr/target/debug/libknn_ocr.d
+++ submit/prj1-sol/knn-ocr/target/debug/libknn_ocr.d
@@ -1,1 +1,1 @@
-/home/nsomu1/projects/cs551/projects/prj1/prj1-sol/knn-ocr/target/debug/libknn_ocr.rlib: /home/nsomu1/projects/cs551/projects/prj1/prj1-sol/knn-ocr/src/lib.rs
+/home/nsomu1/projects/i551/submit/prj1-sol/knn-ocr/target/debug/libknn_ocr.rlib: /home/nsomu1/projects/i551/submit/prj1-sol/knn-ocr/src/lib.rs
